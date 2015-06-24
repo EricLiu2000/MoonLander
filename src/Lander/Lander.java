66be01ec6b068ab1473a6a4be4798b82719763ca
Eric Liu <ericxliu@gmail.com>
2015-06-24 15:33:22 -0700

    progress on lander class

--- src/Lander/Lander.java
+++ src/Lander/Lander.java
@@ -6,9 +6,27 @@ public class Lander
 {
 	private Position pos;
 	private Vector vec;
+	
+	
 	public Lander()
 	{
 		pos = new Position(200, 200);
 		vec = new Vector(0, 0);
+	}
+	
+	public Lander(Position pos, Vector vec)
+	{
+		this.pos = pos;
+		this.vec = vec;
+	}
+	
+	public Lander getLander()
+	{
+		return this;
+	}
+	
+	public void update()
+	{
+		
 	}
 }
