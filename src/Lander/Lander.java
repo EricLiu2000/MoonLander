fb11c2dd51c8acc11f3da09948167384ad1bb6c9
Eric Liu <ericxliu@gmail.com>
2015-06-24 12:48:53 -0700

    added a lander and position class

--- src/Lander/Lander.java
+++ src/Lander/Lander.java
@@ -1,9 +1,14 @@
 package Lander;
 
+import physics.*;
+
 public class Lander 
 {
+	private Position pos;
+	private Vector vec;
 	public Lander()
 	{
-		
+		pos = new Position(200, 200);
+		vec = new Vector(0, 0);
 	}
 }
--- src/physics/Position.java
+++ src/physics/Position.java
@@ -16,5 +16,26 @@ public class Position
 		this.x = x;
 		this.y = y;
 	}
+	
+	public void setPosition(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Position getPosition()
+	{
+		return this;
+	}
+	
+	public void setX(double x)
+	{
+		this.x = x;
+	}
+	
+	public void setY(double y)
+	{
+		this.y = y;
+	}
 
 }
