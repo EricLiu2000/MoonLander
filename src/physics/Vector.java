9b344fbe6140ed8366296d1d6ab543e41479c27f
Eric Liu <ericxliu@gmail.com>
2015-06-24 15:33:02 -0700

    added rotate methods

--- src/physics/Vector.java
+++ src/physics/Vector.java
@@ -2,6 +2,8 @@ package physics;
 
 public class Vector 
 {
+	final static double ROTATE_CONSTANT = 0.5;
+	
 	private double x;
 	private double y;
 	
@@ -21,6 +23,70 @@ public class Vector
 	{
 		this.x += dx;
 		this.y += dx;
+	}
+	
+	public void rotateCounterclockwise()
+	{
+		if(/*SOME KEY PRESSED*/true)
+		{	
+			
+			//tests to see if it is upper quadrant
+			if(this.y <= 0 && Math.abs(this.x) <= -this.y)
+			{
+				this.x = this.x - ROTATE_CONSTANT;
+			}
+			
+			//tests to see if it is right quadrant
+			if(this.x >= 0 && Math.abs(this.y) <= this.x)
+			{
+				this.y = this.y - ROTATE_CONSTANT;
+			}
+			
+			//tests to see if it is bottom quadrant
+			if(this.y >= 0 && Math.abs(this.x) <= this.y)
+			{
+				this.x = this.x + ROTATE_CONSTANT;
+			}
+			
+			//tests to see if it is left quadrant
+			if(this.x <= 0 && Math.abs(this.y) <= -this.x)
+			{
+				this.y = this.y + ROTATE_CONSTANT;
+			}
+		}
+			
+	}
+	
+	public void rotateClockwise()
+	{
+		if(/*SOME KEY PRESSED*/true)
+		{	
+			
+			//tests to see if it is upper quadrant
+			if(this.y <= 0 && Math.abs(this.x) <= -this.y)
+			{
+				this.x = this.x + ROTATE_CONSTANT;
+			}
+			
+			//tests to see if it is right quadrant
+			if(this.x >= 0 && Math.abs(this.y) <= this.x)
+			{
+				this.y = this.y + ROTATE_CONSTANT;
+			}
+			
+			//tests to see if it is bottom quadrant
+			if(this.y >= 0 && Math.abs(this.x) <= this.y)
+			{
+				this.x = this.x - ROTATE_CONSTANT;
+			}
+			
+			//tests to see if it is left quadrant
+			if(this.x <= 0 && Math.abs(this.y) <= -this.x)
+			{
+				this.y = this.y - ROTATE_CONSTANT;
+			}
+			
+		}
 	}
 	
 	public Vector getVector()
