85a7ff181868f18004f7009224bffbe66e6f60c2
Eric Liu <ericxliu@gmail.com>
2015-06-30 10:07:17 -0700

    made a moon

--- src/terrain/Moon.java
+++ src/terrain/Moon.java
@@ -1,11 +1,44 @@
 package terrain;
-import constants.*;
+
+import constants.Constants;
+
+import java.awt.*;
+
 /**
  *
  */
 public class Moon
 {
-    double startingX = Constants.RIGHT_BOUNDS * Math.random();
-    double startingY = 2/3 * Constants.WORLD_HEIGHT * Math.random();
+	public int leftPlatformSide = (int) (20 * Math.random()) + 1;
+	public int platformHeight = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
+	
+	int[] xValues = 
+	{
+			0, 20 , 40, 60, 80, 100, 120, 140, 160, 180, 200,
+			220, 240, 260, 280, 300, 320, 340, 360, 380, 400,
+			420, 440, 460, 480, 500
+	};
+	
+	int[] yValues = new int[26];
 
+    public void createTerrain()
+    {
+        for(int i = 0; i < leftPlatformSide; i++)
+        {
+            yValues[i] = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
+        }
+
+        for(int i = leftPlatformSide; i < leftPlatformSide + 3; i++)
+        {
+            yValues[i] = platformHeight;
+        }
+
+        for(int i = leftPlatformSide + 3; i <= 26; i++)
+        {
+            yValues[i] = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
+        }
+
+        Polygon moon = new Polygon(xValues, yValues, xValues.length);
+
+    }
 }
