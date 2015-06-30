9b1eee69946d3161473df87a981b972d8299e4d3
Eric Liu <ericxliu@gmail.com>
2015-06-30 16:05:11 -0700

    fixed up the moon

--- src/terrain/Moon.java
+++ src/terrain/Moon.java
@@ -11,34 +11,35 @@ public class Moon
 {
 	public int leftPlatformSide = (int) (20 * Math.random()) + 1;
 	public int platformHeight = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
-	
-	int[] xValues = 
-	{
-			0, 20 , 40, 60, 80, 100, 120, 140, 160, 180, 200,
-			220, 240, 260, 280, 300, 320, 340, 360, 380, 400,
-			420, 440, 460, 480, 500
-	};
-	
-	int[] yValues = new int[26];
+    public Polygon moonPolygon;
 
-    public void createTerrain()
+    int[] xValues =
+    	{
+    			0, 20 , 40, 60, 80, 100, 120, 140, 160, 180, 200,
+    			220, 240, 260, 280, 300, 320, 340, 360, 380, 400,
+    			420, 440, 460, 480, 500
+    	};
+
+    int[] yValues = new int[26];
+
+    public Moon()
     {
         for(int i = 0; i < leftPlatformSide; i++)
-        {
-            yValues[i] = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
-        }
+            {
+                    yValues[i] = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
+            }
 
         for(int i = leftPlatformSide; i < leftPlatformSide + 3; i++)
-        {
-            yValues[i] = platformHeight;
-        }
+            {
+                yValues[i] = platformHeight;
+            }
 
         for(int i = leftPlatformSide + 3; i <= 26; i++)
-        {
-            yValues[i] = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
-        }
+            {
+                yValues[i] = (int) (2/3 * Constants.WORLD_HEIGHT * Math.random());
+            }
 
-        Polygon moon = new Polygon(xValues, yValues, xValues.length);
-
+        moonPolygon = new Polygon(xValues, yValues, xValues.length);
     }
+
 }
