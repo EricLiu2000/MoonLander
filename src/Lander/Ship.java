75bc0eb0c1e27f1b9f5749b2c7d7e255a76a4b18
Eric Liu <ericxliu@gmail.com>
2015-06-30 16:11:55 -0700

    added landing speed detection

--- src/Lander/Ship.java
+++ src/Lander/Ship.java
@@ -45,48 +45,50 @@ public class Ship
 
 	public void update()
 	{
-
-        if(this.pos.getX() > Constants.RIGHT_BOUNDS)
+        while(true)
         {
-            this.pos.setX(Constants.LEFT_BOUNDS + 0.25);
-        }
-
-        if(this.pos.getX() < Constants.LEFT_BOUNDS)
-        {
-            this.pos.setX(Constants.RIGHT_BOUNDS - 0.25);
-        }
-        if(collide())
-        {
-            if(fuel > 0)
+            if(this.pos.getX() > Constants.RIGHT_BOUNDS)
             {
-                pos.move(0, Constants.GRAVITY);
-
-                if(KeyboardListener.isKeyPressed('d'))
-                {
-                    vec.rotateClockwise();
-                }
-
-                if(KeyboardListener.isKeyPressed('a'))
-                {
-                    vec.rotateCounterclockwise();
-                }
-
-                if(KeyboardListener.isKeyPressed('w'))
-                {
-                    this.vec.move(this.vec.getX()/(Math.sqrt((this.vec.getX() * this.vec.getX()) + (this.vec.getY() * this.vec.getY()))),
-                            this.vec.getY()/(Math.sqrt((this.vec.getX() * this.vec.getX()) + (this.vec.getY() * this.vec.getY()))));
-
-                    this.pos.move(this.vec);
-                    fuel -= 1;
-                }
-
+                this.pos.setX(Constants.LEFT_BOUNDS + 0.25);
             }
-            else pos.move(0, Constants.GRAVITY);
+
+            if(this.pos.getX() < Constants.LEFT_BOUNDS)
+            {
+                this.pos.setX(Constants.RIGHT_BOUNDS - 0.25);
+            }
+
+            if(!collide() && (Math.sqrt((this.vec.getX() * this.vec.getX()) + (this.vec.getY() * this.vec.getY()))) < Constants.ACCEPTABLE_LANDING_SPEED)
+            {
+                if(fuel > 0)
+                {
+                    pos.move(0, Constants.GRAVITY);
+
+                    if(KeyboardListener.isKeyPressed('d'))
+                    {
+                        vec.rotateClockwise();
+                    }
+
+                    if(KeyboardListener.isKeyPressed('a'))
+                    {
+                        vec.rotateCounterclockwise();
+                    }
+
+                    if(KeyboardListener.isKeyPressed('w'))
+                    {
+                        this.vec.move(this.vec.getX()/(Math.sqrt((this.vec.getX() * this.vec.getX()) + (this.vec.getY() * this.vec.getY()))),
+                                this.vec.getY()/(Math.sqrt((this.vec.getX() * this.vec.getX()) + (this.vec.getY() * this.vec.getY()))));
+
+                        this.pos.move(this.vec);
+                        fuel -= 1;
+                    }
+
+                }
+                else pos.move(0, Constants.GRAVITY);
+            }
+
+            else/*GAME OVER*/;
+
         }
-
-        else/*GAME OVER*/;
-
-
 	}
 
     public boolean collide()
